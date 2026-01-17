5c90bcf2eb7204648eb816ef87d8a91b82462d7f
Tomoyasu Hoshino <hoshi@nkk.ac.jp>
2026-01-17 16:53:44 +0900

    Add: editor-config.jsとeditor-there.cssを追加

---
+++ ui/editor-config.js
@@ -0,0 +1,8 @@
+// ui/editor-config.js
+window.OfflineCodeEditorConfig = {
+  control: ["if","else","repeat","loop","while","until","for","end", "endif", "endrepeat", "endloop", "endwhile", "enduntil", "endfor"],
+  command: ["move","turn","put","reset","mask","base","qrcode", "hello", "helloworld"],
+  predicateWord: ["block","wall","empty","used","timing","skip","last"],
+  switchNames: ["red","blue","green","yellow"],
+  constants: ["left","right","up","down","on","off","flip","toggle","true","false"]
+};
