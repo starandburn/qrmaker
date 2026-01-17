6f1807e511723545c43957ee90f2b494fc54fd87
Tomoyasu Hoshino <hoshi@nkk.ac.jp>
2026-01-17 22:25:27 +0900

    Update: 行番号ガターの最小幅を確保して表示の安定性を向上

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 window.appVersion = {
   major: 2,
   minor: 0,
-  revision: 0,
+  revision: 5,
 };
 window.appVersionString = `${window.appVersion.major}.${window.appVersion.minor}.${window.appVersion.revision}`;
