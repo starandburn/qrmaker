7f329aa67221ed0496c3a29c147fdea2598a0d31
Tomoyasu Hoshino <hoshi@nkk.ac.jp>
2026-01-17 16:38:58 +0900

    Chore: v2系統の開発開始

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 window.appVersion = {
-  major: 1,
-  minor: 4,
-  revision: 12,
+  major: 2,
+  minor: 0,
+  revision: 0,
 };
 window.appVersionString = `${window.appVersion.major}.${window.appVersion.minor}.${window.appVersion.revision}`;
