15029f041ad436918bda9ef3db5577804c06d72d
Tomoyasu Hoshino <hoshi@nkk.ac.jp>
2026-01-20 21:09:10 +0900

    Refactor: internal-api公開内容を役割別に整理して構成と可読性を向上

--- app/internal-api.js
+++ app/internal-api.js
@@ -5,11 +5,15 @@
 
   function createInternalApi(win){
     const internal = {};
+    // Step & Thenable
     assignIfFunction(internal, "shouldStepFunctions", win.shouldStepFunctions);
     assignIfFunction(internal, "makeStepThenable", win.makeStepThenable);
+    // Stepped Drawing
     assignIfFunction(internal, "drawBasePatternsStepped", win.drawBasePatternsStepped);
     assignIfFunction(internal, "drawDataPatternsStepped", win.drawDataPatternsStepped);
+    // Run Control
     assignIfFunction(internal, "stopCurrentRun", win.stopCurrentRun);
+    // Reset / State
     assignIfFunction(internal, "resetCommand", win.resetCommand);
     return internal;
   }
