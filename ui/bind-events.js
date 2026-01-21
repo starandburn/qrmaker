077357b9c8bef01349297c90bcbdc805fb99b68c
hoshi <hoshi@nkk.ac.jp>
2026-01-21 09:29:59 +0900

    Refactor: 生成開始・終了時のUIロックと状態更新処理をbegin/endGenerateClickに集約

--- ui/bind-events.js
+++ ui/bind-events.js
@@ -29,8 +29,50 @@
     }
   }
 
+  function beginGenerateClick(ctx){
+    if(!ctx) return null;
+    const uiState = ctx.uiState;
+    const setExecutionStatus = ctx.setExecutionStatus;
+    const setInputLock = ctx.setInputLock;
+    const lockToken = (typeof global.acquireUiInputLock === "function")
+      ? global.acquireUiInputLock(uiState)
+      : (() => {
+        const fallbackToken = uiState.inputLockToken + 1;
+        uiState.setInputLockToken(fallbackToken);
+        return fallbackToken;
+      })();
+    if(typeof setExecutionStatus === "function"){
+      setExecutionStatus("running");
+    }
+    if(typeof setInputLock === "function"){
+      setInputLock(true);
+    }
+    return { lockToken };
+  }
+
+  function endGenerateClick(ctx, token){
+    if(!ctx || !token) return;
+    const uiState = ctx.uiState;
+    const setInputLock = ctx.setInputLock;
+    const lockToken = token.lockToken;
+    if(lockToken === uiState.inputLockToken && typeof setInputLock === "function"){
+      setInputLock(false);
+    }
+    if(typeof global.releaseUiInputLockIfMatched === "function"){
+      global.releaseUiInputLockIfMatched(uiState, lockToken);
+    }else if(lockToken === uiState.inputLockToken){
+      uiState.clearInputLockToken();
+    }
+  }
+
   if(typeof global.bindSimpleUiEvents !== "function"){
     global.bindSimpleUiEvents = bindSimpleUiEvents;
+  }
+  if(typeof global.beginGenerateClick !== "function"){
+    global.beginGenerateClick = beginGenerateClick;
+  }
+  if(typeof global.endGenerateClick !== "function"){
+    global.endGenerateClick = endGenerateClick;
   }
 
   if(typeof global.acquireUiInputLock !== "function"){
